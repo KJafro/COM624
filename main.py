c7c159bb530d03dbf1ff286c563d5d1cb845add4
KJafro <Kurtj222@outlook.com>
2022-12-24 10:37:59 +0000

    Streamlit Title and Input Stocks

--- main.py
+++ main.py
@@ -20,3 +20,38 @@ def load_data(ticker):
     data.reset_index(inplace=True)
     stock_data = yf.Ticker(ticker)
     return (data, stock_data)
+
+st.markdown(
+    """<p style="text-align: center;"><span style="color: rgb(44, 130, 201); font-size: 50px;"><strong>Stock Predictor COM624</strong></span></p>""",
+    unsafe_allow_html=True)
+st.markdown(
+    """<p style="text-align: center; font-size: 22px;">Instructions:</p>""", unsafe_allow_html=True)
+st.markdown(
+"""<p style="text-align: center; font-size: 18px;">
+1) Select the Sector of the Stock you wish to view
+<br>
+2) Enter the start date you wish to view the stock from
+<br>
+3) Select Stock from the dropdown list
+<br>
+4) Scroll to the bottom and click the 'Predict' button</p>""", unsafe_allow_html=True)
+
+st.write('---')
+st.sidebar.markdown(
+    """<p style="
+     text-align: center;
+     font-family: Helvetica;
+     font-weight: 500; 
+     padding: 130px;
+     color: rgb(44, 130, 201); 
+     font-size: 34px;
+     "></p>""", unsafe_allow_html=True)
+
+df = load_stocks()
+sector = df.groupby('GICS Sector')
+sorted_sector = sorted(df['GICS Sector'].unique())
+selected_multi = st.sidebar.multiselect('**Sector**',sorted_sector)
+df_selected_multi = df[df['GICS Sector'].isin(selected_multi)]
+
+START = st.sidebar.date_input(label="**Enter Start Date**",value=datetime.date(2018,1,1))
+TODAY = datetime.date.today().strftime("%Y-%m-%d")
